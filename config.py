600c5affaa88a04873c6fd98b8298dbd2d02b6f5
Skyduy <cuteuy@gmail.com>
2019-08-02 18:28:19 +0800

    mod for 80000 trains

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ EPSILON_HIGH = 0.5  # starting value of epsilon
 EPSILON_LOW = 0.01  # final value of epsilon
 REPLAY_SIZE = 20000  # experience replay buffer size
 BATCH_SIZE = 256  # size of minibatch
-DECAY = 1000  # epsilon decay config  1000 for 8000, 50000 for 40000
-UPDATE_TARGET_EVERY = 20  # target-net参数更新频率
+DECAY = int((80000 * (2/3)) / 5)  # epsilon decay config  1000 for 80000
+UPDATE_TARGET_EVERY = 50  # target-net参数更新频率
 
 CARDS = range(3, 18)
 STR = [str(i) for i in range(3, 11)] + ['J', 'Q', 'K', 'A', '2', '小', '大']
---
+++ utils.py
@@ -0,0 +1,27 @@
+import json
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter1d
+
+sns.set(color_codes=True)
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+
+def plot(fn):
+    with open('outs/{}.json'.format(fn)) as f:
+        data = json.load(f)
+    y = np.array(data) / 100
+    plt.plot(y, alpha=0.3)
+    sm = gaussian_filter1d(y, sigma=5)
+    plt.plot(sm)
+    plt.title('地主胜率走势')
+    plt.xlabel('训练总百次数')
+    plt.ylabel('过去100次AI地主胜率')
+    plt.savefig('images/{}.svg'.format(fn), format='svg')
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot('0802_0812')
