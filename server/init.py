45c4b9387ca4524158fd21515db28463f31e2de4
Skyduy <cuteuy@gmail.com>
2019-08-09 19:58:43 +0800

    bug fix

--- server/init.py
+++ server/init.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+cur_dir, _ = os.path.split(os.path.abspath(__file__))
+par_dir = os.path.abspath(os.path.join(cur_dir, '..'))
+sys.path.insert(0, par_dir)
+
 from server.app import db
 
 db.create_all()
